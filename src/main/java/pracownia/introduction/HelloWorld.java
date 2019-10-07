230bc6b525a58f2138a58901d3cb7061aa86e9b7
s444461 <lila0073@wp.pl>
2019-10-07 12:57:09 +0200

    pierwszy comit

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        Logger log = Logger.getLogger("cd");
+        log.info("sisis");
+        log.error("erorrrr");
     }
 }
